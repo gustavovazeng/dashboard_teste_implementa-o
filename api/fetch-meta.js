8f58b1786651ab4ae47608a5f305fd617d6ba1cd
gustavovazeng <gustavo.vaz.eng@gmail.com>
2026-06-27 20:55:16 -0300

    Update fetch-meta.js

--- api/fetch-meta.js
+++ api/fetch-meta.js
@@ -16,9 +16,7 @@ export default async function handler(req, res) {
     'campaign_name'
   ].join(',');
 
-  const yesterday = new Date();
-  yesterday.setDate(yesterday.getDate() - 1);
-  const dateStr = yesterday.toISOString().split('T')[0];
+const dateStr = '2026-06-26';
 
   let allRows = [];
 
